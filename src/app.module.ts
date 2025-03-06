a90ec17b20be4519385f75f1faba7d96fbd831af
IpekoGosu <156728052+IpekoGosu@users.noreply.github.com>
2025-03-06 22:57:54 +0900

    fix: added missing bracket

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
-import { CommonErrorFilter, HttpErrorFilter } from '@src/common/filter/error.filter';
+import {
+  CommonErrorFilter,
+  HttpErrorFilter,
+} from '@src/common/filter/error.filter';
 
 @Module({
   imports: [],
@@ -13,6 +16,6 @@ import { CommonErrorFilter, HttpErrorFilter } from '@src/common/filter/error.fil
       provide: APP_FILTER,
       useClass: HttpErrorFilter,
     },
-  
+  ],
 })
 export class AppModule {}
