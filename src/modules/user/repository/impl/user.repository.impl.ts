26e01af660d40ad77d761bf5cab6e82b0c884f3d
IpekoGosu <156728052+IpekoGosu@users.noreply.github.com>
2025-03-06 23:01:26 +0900

    add: error handling to user repository

--- src/modules/user/repository/impl/user.repository.impl.ts
+++ src/modules/user/repository/impl/user.repository.impl.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient, user } from '@prisma/client';
+import { CommonError } from '@src/common/error/common.error';
+import { ERROR_STATUS } from '@src/common/error/error.status';
 import { UserCreateDto } from '@src/modules/user/dto/user.create.dto';
 import { UserRepository } from '@src/modules/user/repository/user.repository';
 
@@ -8,6 +10,11 @@ export class UserRepositoryImpl implements UserRepository {
   constructor(private prisma: PrismaClient) {}
 
   async create(userCreateDto: UserCreateDto): Promise<user> {
-    return this.prisma.user.create({ data: userCreateDto });
+    try {
+      return this.prisma.user.create({ data: userCreateDto });
+    } catch (error) {
+      console.error(error);
+      throw new CommonError(ERROR_STATUS.DB_INSERT_ERROR);
+    }
   }
 }
